94fcad820e1c0bd05adee892bac50813275b1f60
ClifAIsla <clifa.isla@gmail.com>
2022-01-10 12:47:43 -0500

    evento on click

---
+++ script.js
@@ -0,0 +1,9 @@
+function cambiartexto(elemto){
+    elemto.innerText = "Logout";
+}
+function removebutton(elemto){
+    elemto.remove();
+}
+function mostrar(elemto){
+    alert('Ninja was liked');
+}
